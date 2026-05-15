31b265b3dad26248ab497b533a37a7c633f91337
Alex <alexalexbabarika@gmail.com>
2026-05-15 16:26:16 +0100

    feat: rename cache config into data config and expand it, add train config

--- config.py
+++ config.py
@@ -16,11 +16,25 @@ class AudioConfig:
 
 
 @dataclass
-class CacheConfig:
+class DataConfig:
     cache_dir: str = "./cache"
 
     nsynth_hf_dataset_name: str = "jg583/NSynth"
+    nsynth_subdir_name: str = "nsynth"
     nsynth_length_hr: int = 2
 
     vctk_hf_dataset_name: str = "sanchit-gandhi/vctk"
+    vctk_subdir_name: str = "vctk"
     vctk_length_hr: int = 2
+
+    datasets_to_load = ["nsynth", "vctk"]
+    val_split: float = 0.02
+
+    assert val_split < 1
+
+
+@dataclass
+class TrainConfig:
+    perturb_st: int = 2
+    batch_size: int = 32
+    num_workers: int = 6
